f22b9503c9048d28ac09cef1e96ee695819ea10a
Kei Chan 'Fuwa <116655254+Fuwa-Kei@users.noreply.github.com>
2023-02-17 15:31:01 +0000

    functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,35 +1,36 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = 20;
-var circ = function (diameter) {
+"use strict";
+let character = 'mario';
+let age = 30;
+let isBlackBelt = 20;
+const circ = (diameter) => {
     return diameter * Math.PI;
 };
 // argument must be number 
 console.log(circ(10));
-var names = ['Luigi', 'Mario', 'Yoshi'];
+let names = ['Luigi', 'Mario', 'Yoshi'];
 names.push('Toad');
 // if an array of strings then can only add/replace with strings 
-var mixed = ['ken', 4, 'chun-li', 9, true];
+let mixed = ['ken', 4, 'chun-li', 9, true];
 // mixed arrays allowed as log as pre specified 
 // now can replace string with a number
 mixed[0] = 3;
 // whan you decalare anything as a specific type that cannot be changed 
-var ninja = {
+let ninja = {
     name: 'hanabi',
     type: 'marksman',
     kills: 5
 };
 // declare an array with its type + the array 
 // ninja is an arrays of strings, starting with an empty string
-var ninjas = [];
+let ninjas = [];
 ninjas.push('hayabusa');
 // can have mixed types in array (union types)
-var mixedArrary = [];
+let mixedArrary = [];
 mixedArrary.push('cyclops');
 mixedArrary.push(3);
 // objects
-var hanabi;
+let hanabi;
 hanabi = { role: 'marksman', skills: ['fan', 'ulti'] };
 // any types
-var hero = 'natan';
+let hero = 'natan';
 hero = true;
