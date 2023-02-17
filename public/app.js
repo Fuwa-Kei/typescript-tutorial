8b16033395772bec339435dcdaacae91e1a9b80f
Kei Chan 'Fuwa <116655254+Fuwa-Kei@users.noreply.github.com>
2023-02-17 18:14:00 +0000

    classes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,26 @@
 "use strict";
+// classes
+// initialise values in a contructor and assign them to the values
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+// create an object using the class
+const invOne = new Invoice('mario', 'worked on the mario website', 250);
+const invTwo = new Invoice('Luigi', 'worked on the luigi website', 300);
+// can change what the propeties are 
+invOne.client = 'yoshi';
+// all invoices created using the invoice class can be added
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 // grab an anchor tag - the ! tells it that there is an anchor tag on the page 
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
